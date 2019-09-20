1f6c04c1087900800391172f11d362106c66c03f
Johanes Neumann <johanes.neumann@gmail.com>
2019-09-20 00:51:09 -0300

    Componente para header e footer, icone para favoritar um pokemon, fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { PokemonPageComponent } from './pokemon-page/pokemon-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import { PaginatorComponent } from './common/components/paginator/paginator.component';
+import { FooterComponent } from './common/components/footer/footer.component';
+import { HeaderComponent } from './common/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonPageComponent,
     PokemonDetailComponent,
-    PaginatorComponent
+    PaginatorComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
